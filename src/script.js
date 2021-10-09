c694f15af0e829ed6a3814ecc80f3cc3a568056a
Lalemong <92227430+Lalemong@users.noreply.github.com>
2021-10-10 03:16:38 +0900

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,93 @@
+const $yes = document.querySelector("#yes");
+const $no = document.querySelector("#no");
+const $popup = document.querySelector("#popup");
+const $game = document.querySelector("#game");
+const $pass = document.querySelector("#pass");
+const $pad = document.querySelector("#pad");
+const $ex = document.querySelector("#ex");
+const $reset = document.querySelector("#reset");
+const $retry = document.querySelector("#retry");
+const number = [0,1,2,3,4,5,6,7,8,9];
+let answer = "";
+let sheet = "";
+let closePopup = false;
+
+const gameStart = e => {
+  const btns = $pad.querySelectorAll(".btn");
+  btns.forEach(el => el.remove());
+  $pass.value = "";
+  sheet = "";
+  
+  number.sort(e => Math.random() - 0.5);
+  answer = number.join("");
+  $ex.innerText = answer;
+
+  number.sort(e => Math.random() - 0.5);
+  for(let i = 0; i < 10; i++) {
+    const btn = document.createElement("div");
+    btn.classList.add("btn");
+    btn.innerText = number[i];
+
+    btn.addEventListener("click", e => {
+      sheet += `${number[i]}`;
+      $pass.value = sheet;
+      btn.classList.add("use");
+
+      if(sheet.length === 10) {
+        if(sheet === answer) {
+          location.replace("https://twip.kr/l_l_mong1122");
+        }else {
+          $reset.classList.remove("none");
+          $game.style.pointerEvents = "none";
+        }
+      }
+    })
+
+    $pad.append(btn);
+  }
+}
+
+$no.addEventListener("click", e => {
+  window.close();
+})
+
+$yes.addEventListener("click", e => {
+  $popup.classList.remove("none");
+
+  gameStart();
+})
+
+$pass.addEventListener("keydown", e => {
+  e.preventDefault();
+  $pass.value = sheet;
+})
+
+$pass.addEventListener("input", e => {
+  e.preventDefault();
+  $pass.value = sheet;
+})
+
+$pass.addEventListener("contextmenu", e => {
+  e.preventDefault();
+  $pass.value = sheet;
+})
+
+$popup.addEventListener("mousedown", e => {
+  if(e.target === $popup) {
+    closePopup = true;
+  }
+});
+
+$popup.addEventListener("mouseup", e => {
+  if(e.target === $popup && closePopup) {
+    $popup.classList.add("none");
+  }
+  closePopup = false;
+});
+
+$retry.addEventListener("click", e => {
+  $reset.classList.add("none");
+  $game.style.pointerEvents = "";
+
+  gameStart();
+})
